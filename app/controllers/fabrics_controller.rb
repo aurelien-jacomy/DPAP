3663997adc68e9b56bcbb81bd5a3d09a495cd262
Gabriel Antonini <gabriel.antonini@gmail.com>
2020-05-23 11:53:16 -0300

    routes and controller ok. missing view

--- app/controllers/fabrics_controller.rb
+++ app/controllers/fabrics_controller.rb
@@ -1,6 +1,17 @@
 class FabricsController < ApplicationController
+	before_action :set_fabric, only: [ :show ]
 
     def index
         @fabrics = policy_scope(Fabric) 
+    end
+
+    def show
+    end
+
+    private
+
+    def set_fabric
+    	@fabric = Fabric.find(params[:id])
+    	authorize @fabric
     end
 end
--- app/policies/fabric_policy.rb
+++ app/policies/fabric_policy.rb
@@ -4,4 +4,8 @@ class FabricPolicy < ApplicationPolicy
       scope.all 
     end
   end
+
+  def show?
+  	return true
+  end
 end
