d67d858e2502d785ae3fc3b2a808df8033347613
victor albanezi <victor.albanezi@hotmail.com>
2020-05-23 18:56:21 -0300

    comment removed

--- app/models/fabric.rb
+++ app/models/fabric.rb
@@ -12,7 +12,7 @@ class Fabric < ApplicationRecord
   pg_search_scope :search_by_name_colour_width_gramatura_composition_and_fabric_type,
     against: [ :name, :colour, :width, :gramatura, :composition, :fabric_type ],
     using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+      tsearch: { prefix: true } 
     }
 
 
