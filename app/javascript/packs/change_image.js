9992050db1d9fcff80832de5708c18c793eaacc8
Gabriel Antonini <gabriel.antonini@gmail.com>
2020-06-04 21:28:52 -0300

    stil wip

--- app/javascript/packs/change_image.js
+++ app/javascript/packs/change_image.js
@@ -1,1 +1,12 @@
-const display
+const initChangeImage = () => {
+	bigPic = document.getElementById("main-pic");
+	let smallPics = [];
+	smallPics = document.querySelectorAll(".small-pics");
+	smallPics.forEach((smallPic) => {
+		smallPic.addEventListener("click", (event) => {
+			togglePics(smallPic, bigPic);
+		});
+	});
+}
+
+const togglePics = (smallPic, bigPic) => {}
