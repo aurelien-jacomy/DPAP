0f7a1b0ce384051c456b28e1149ebdbef1d5d803
Aurélien Jacomy <jacomya@gmail.com>
2020-06-14 16:57:21 +0200

    Frontend working. Missing query for results

--- app/controllers/fabrics_controller.rb
+++ app/controllers/fabrics_controller.rb
@@ -8,13 +8,19 @@ class FabricsController < ApplicationController
         @fabric_labels = Label.select(:name).distinct.map {|x| x.name}
         @fabric_to_cart = FabricToCart.new
         
-    if params[:search].nil?
-        @fabrics = policy_scope(Fabric)
-         elsif params[:search][:query].blank?
+        if params[:search].nil?
+            @fabrics = policy_scope(Fabric)
+        elsif params[:search][:query].blank?
             @fabrics = policy_scope(Fabric)
         else
             @query = params[:search][:query]
             @fabrics = policy_scope(Fabric.search_by_name_colour_width_gramatura_composition_and_fabric_type("#{@query}")) 
+        end
+
+        if params[:label_search]
+            label_id = params[:label_search][:label_id]
+            @fabrics = policy_scope(Fabric).joins(:label_to_fabrics).where('label_to_fabric.label_id = ?', label_id)
+            @query = Label.find(label_id)
         end
     end
 
--- app/javascript/components/label.js
+++ app/javascript/components/label.js
@@ -2,7 +2,7 @@ const showLabel = () => {
 	let labels = [];
 	labels = document.querySelectorAll(".card-label");
 	labels.forEach((label) => {
-		label.querySelector(".card-label-header").addEventListener( "click", (event) => {
+		label.querySelector(".card-label-header-content").addEventListener( "click", (event) => {
 			label.querySelector(".card-label-content").toggleAttribute("hidden");
 		})
 	});
